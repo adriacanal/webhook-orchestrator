29a74425da1730052f1fd6a7fb9aa391c474f556
Adrià Canal <hola@adriacanal.com>
2026-01-31 01:01:58 +0100

    style: fix code formatting with Pint

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StripeWebhookController;
+use Illuminate\Support\Facades\Route;
 
 Route::post('/webhooks/stripe', StripeWebhookController::class);
